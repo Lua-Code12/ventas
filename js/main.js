f1ea1296d052957c32e0fe3b32deb28b6aab3548
Lua-Code12 <camila.valdi.r@gmail.com>
2020-05-11 17:39:46 -0400

    creación de formulario

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/**first slider**/
+/**Primer slider**/
 $(".slider-one")
     .not(".slick-intialized")
     .slick({
@@ -7,4 +7,31 @@ $(".slider-one")
     dots:true,
     preArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next",
+});
+
+
+/**Segundo slider**/
+
+$(".slider-two")
+    .not(".slick-intialized")
+    .slick({
+    preArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
+});
+
+
+/**tercer slider**/
+
+$(".slider-three")
+    .not(".slick-intialized")
+    .slick({
+    preArrow: ".site-slider-three .prev",
+    nextArrow: ".site-slider-three .next",
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+    infinite: false
 });
